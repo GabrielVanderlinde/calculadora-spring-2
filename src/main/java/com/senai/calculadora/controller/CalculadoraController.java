f5d64851ae8e0c3b7386770a19b1fac36974cff0
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-03-02 20:35:33 -0300

    refactor: cria estrutura passando parametros para o controller

--- src/main/java/com/senai/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/senai/calculadora/controller/CalculadoraController.java
@@ -1,7 +1,9 @@
 package com.senai.calculadora.controller;
 
+import com.senai.calculadora.dto.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,23 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculadoraController {
 
     @PostMapping("/adicionar")
-    public ResponseEntity<String> adicionar() {
+    public ResponseEntity<ResultadoDto> adicionar(@RequestBody EntradaDto dados) {
         return ResponseEntity.ok().body("");
     }
 
     @PostMapping("/subtrair")
-    public ResponseEntity<String> subtrair() {
+    public ResponseEntity<ResultadoDto> subtrair(@RequestBody EntradaDto dados) {
         return ResponseEntity.ok().body("");
     }
 
 
     @PostMapping("/multiplicar")
-    public ResponseEntity<String> multiplicar() {
+    public ResponseEntity<ResultadoDto> multiplicar(@RequestBody EntradaDto dados) {
         return ResponseEntity.ok().body("");
     }
 
     @PostMapping("/dividir")
-    public ResponseEntity<String> dividir() {
+    public ResponseEntity<ResultadoDto> dividir(@RequestBody EntradaDto dados) {
         return ResponseEntity.ok().body("");
     }
 }
