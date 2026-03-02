ae356e4abbc56a878221aa0fab19ffac661f669e
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-03-02 20:52:44 -0300

    refactor: cria estrutura base service

--- src/main/java/com/senai/calculadora/service/CalculadoraService.java
+++ src/main/java/com/senai/calculadora/service/CalculadoraService.java
@@ -1,28 +1,25 @@
 package com.senai.calculadora.service;
 
+import com.senai.calculadora.dto.EntradaDto;
+import com.senai.calculadora.dto.ResultadoDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculadoraService {
 
-    public double adicao(double n1, double n2) {
-        return n1 + n2;
+    public ResultadoDto adicao(EntradaDto dados) {
+        return new ResultadoDto();
     }
 
-    public double subtracao(double n1, double n2) {
-        return n1 - n2;
+    public ResultadoDto subtracao(EntradaDto dados) {
+        return new ResultadoDto();
     }
 
-    public double multiplicacao(double n1, double n2) {
-        return n1 * n2;
+    public ResultadoDto multiplicacao(EntradaDto dados) {
+        return new ResultadoDto();
     }
 
-    public double divisao(double n1, double n2) {
-        if (n1 == 0 || n2 == 0) {
-            throw new IllegalArgumentException("Divisão por Zero Impossível");
-        }
-        return n1 / n2;
+    public ResultadoDto divisao(EntradaDto dados) {
+        return new ResultadoDto();
     }
-
-
 }
