8201ed2496e2aa5eec35e7c9ed977fc64db8f526
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-03-03 21:12:48 -0300

    feat: implementa simplificacao das classes dto atraves da integracao Lombock

--- src/main/java/com/senai/calculadora/dto/EntradaDto.java
+++ src/main/java/com/senai/calculadora/dto/EntradaDto.java
@@ -1,30 +1,15 @@
 package com.senai.calculadora.dto;
 
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class EntradaDto {
     private Double numero1;
     @Min(value = 1, message = "Impossível Divisão por Zero!")
     private Double numero2;
 
-    //Contructor
-    public EntradaDto() {
-    }
-
-    //Get And Setter
-    public Double getNumero1() {
-        return numero1;
-    }
-
-    public void setNumero1(Double numero1) {
-        this.numero1 = numero1;
-    }
-
-    public Double getNumero2() {
-        return numero2;
-    }
-
-    public void setNumero2(Double numero2) {
-        this.numero2 = numero2;
-    }
+    private String operacao;
 }
--- src/main/java/com/senai/calculadora/dto/ResultadoDto.java
+++ src/main/java/com/senai/calculadora/dto/ResultadoDto.java
@@ -1,37 +1,14 @@
 package com.senai.calculadora.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ResultadoDto {
 
     private Double numero1;
     private Double numero2;
+    private String operacao;
     private Double resultado;
-
-    //Constructor
-    public ResultadoDto() {
-    }
-
-    //Get and Set
-    public Double getNumero1() {
-        return numero1;
-    }
-
-    public void setNumero1(Double numero1) {
-        this.numero1 = numero1;
-    }
-
-    public Double getNumero2() {
-        return numero2;
-    }
-
-    public void setNumero2(Double numero2) {
-        this.numero2 = numero2;
-    }
-
-    public Double getResultado() {
-        return resultado;
-    }
-
-    public void setResultado(Double resultado) {
-        this.resultado = resultado;
-    }
 }
