c8a77af045f835175dd418f2dabea89d54e41d9d
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-03-09 17:59:33 -0300

    feat: centraliza operações no endpoint único /calculadora

--- src/main/java/com/senai/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/senai/calculadora/controller/CalculadoraController.java
@@ -3,6 +3,8 @@ package com.senai.calculadora.controller;
 import com.senai.calculadora.dto.*;
 import com.senai.calculadora.service.CalculadoraService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,33 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculadora")
+@AllArgsConstructor
 public class CalculadoraController {
-
-    //Injeção de Dependencias Service
     final CalculadoraService service;
 
-    public CalculadoraController(CalculadoraService service) {
-        this.service = service;
-    }
-
-    public ResponseEntity<ResultadoDto> adicionar(@RequestBody EntradaDto dados) {
-        ResultadoDto resultado = service.adicionar(dados);
-        return ResponseEntity.ok().body(resultado);
-    }
-
-    public ResponseEntity<ResultadoDto> subtrair(@RequestBody EntradaDto dados) {
-        ResultadoDto resultado = service.subtrair(dados);
-        return ResponseEntity.ok().body(resultado);
-    }
-
-
-    public ResponseEntity<ResultadoDto> multiplicar(@RequestBody EntradaDto dados) {
-        ResultadoDto resultado = service.multiplicar(dados);
-        return ResponseEntity.ok().body(resultado);
-    }
-
-    public ResponseEntity<ResultadoDto> dividir(@RequestBody @Valid EntradaDto dados) {
-        ResultadoDto resultado = service.dividir(dados);
+    @PostMapping
+    public ResponseEntity<ResultadoDto> calcular(@RequestBody EntradaDto dados) {
+        ResultadoDto resultado = service.calcular(dados);
         return ResponseEntity.ok().body(resultado);
     }
 }
